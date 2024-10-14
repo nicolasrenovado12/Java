68a14696f5bbe4eeef29754118fdfbf365d8e9b7
Nicolas <nicolasrenovado12@gmail.com>
2024-10-14 10:25:53 -0300

    Adicionando programa básico da composição para ilustrar aprendizado

--- curso_programacao/section_12_Enumerações_E_Composições/composição_123/Carro.java
+++ curso_programacao/section_12_Enumerações_E_Composições/composição_123/Carro.java
@@ -1,5 +1,18 @@
 package composição_123;
 
-
-
-public class Carro
+public class Carro {
+	
+	String cor;
+	String marca;
+	Motor motor;
+	
+	public Carro(String cor, String marca, Motor motor) {
+		this.cor = cor;
+		this.marca = marca;
+		this.motor = motor;
+	}
+	
+	
+	
+	
+}
--- curso_programacao/section_12_Enumerações_E_Composições/composição_123/Program.java
+++ curso_programacao/section_12_Enumerações_E_Composições/composição_123/Program.java
@@ -4,10 +4,18 @@ import java.util.Scanner;
 
 public class Program {
 	public static void main(String[] args) {
+		
+		// This program is difference than course.
+		
 		Scanner sc = new Scanner(System.in);
 
-		Carro carro = new Carro();
+		
+		Motor motorv8 = new Motor(8, 560);
+		Carro carro = new Carro("Azul", "Volkswagen", motorv8);
 
+		
+		System.out.println("Informações do carro abaixo: ");
+		System.out.println(carro.toString());
 		
 
 		sc.close();
