9a8168cf3518c0bf1634188944aab1d88cb88f5a
Nicolas <nicolasrenovado12@gmail.com>
2024-10-14 10:27:00 -0300

    Corrijindo erro básico do programa de composição

--- curso_programacao/section_12_Enumerações_E_Composições/composição_123/Carro.java
+++ curso_programacao/section_12_Enumerações_E_Composições/composição_123/Carro.java
@@ -2,15 +2,20 @@ package composição_123;
 
 public class Carro {
 	
-	String cor;
-	String marca;
-	Motor motor;
+	private String cor;
+	private String marca;
+	private Motor motor;
 	
 	public Carro(String cor, String marca, Motor motor) {
 		this.cor = cor;
 		this.marca = marca;
 		this.motor = motor;
 	}
+
+	public String toString() {
+		return "Carro [cor=" + cor + ", marca=" + marca + ", motor=" + motor.toString() + "]";
+	}
+	
 	
 	
 	
--- curso_programacao/section_12_Enumerações_E_Composições/composição_123/Motor.java
+++ curso_programacao/section_12_Enumerações_E_Composições/composição_123/Motor.java
@@ -2,6 +2,28 @@ package composição_123;
 
 public class Motor {
 
-    int cavalos;
+	private int cilindros;
+    private int cavalos;
+    
 
+	public Motor(int cilndros, int cavalos) {
+		this.cilindros = cilindros;
+		this.cavalos = cavalos;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Motor [cilindros=" + cilindros + ", cavalos=" + cavalos + "]";
+	}
+
+	
+	
+
+
+    
+    
+
+    
+    
 }
