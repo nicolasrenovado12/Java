22ccbe9ff70ea6c1b7b9d536e45ce0c60d86287c
Nicolas Guilherme <159655443+nicolasrenovado12@users.noreply.github.com>
2024-11-19 11:54:06 -0300

    Update comments

--- projetos/encontrando_Coordenadas_Cobra/application/Configuration_Program.java
+++ projetos/encontrando_Coordenadas_Cobra/application/Configuration_Program.java
@@ -7,7 +7,7 @@ public class Configuration_Program {
 	public static void main(String[] args) {
 		ArrayList<String> grid = new ArrayList<String>();
 
-		// Irá ser mostrado por (x, y) por ser um padrão matemático.
+		// Irá ser mostrado por (x, y) por ser um padrão matemático de gráficos.
 		
 		grid.add(">>v");
 		grid.add("^h<");
