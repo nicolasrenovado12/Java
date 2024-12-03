c30ad9fc8fb140020f428d6a3449fcaf8084a772
Nicolas <nicolasrenovado12@gmail.com>
2024-12-03 14:35:38 -0300

    Sollution of program with interface

--- curso_programacao/section_16/interfaces_165/model/services/RentalService.java
+++ curso_programacao/section_16/interfaces_165/model/services/RentalService.java
@@ -10,9 +10,9 @@ public class RentalService {
 	private Double pricePerHour;
 	private Double pricePerDay;
 	
- 	private BrasilTaxServices taxService;
+ 	private TaxService taxService;
 
-	public RentalService(Double pricePerHour, Double pricePerDay, BrasilTaxServices taxService) {
+	public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
 		this.pricePerHour = pricePerHour;
 		this.pricePerDay = pricePerDay;
 		this.taxService = taxService;
