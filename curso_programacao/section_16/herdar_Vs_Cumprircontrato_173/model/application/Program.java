ec832efd72c39dfa8dc2b5b5fdcea206c9dd00c0
Nicolas <nicolasrenovado12@gmail.com>
2024-12-10 18:32:10 -0300

    Solution with interface and abstract class

--- curso_programacao/section_16/herdar_Vs_Cumprircontrato_173/model/application/Program.java
+++ curso_programacao/section_16/herdar_Vs_Cumprircontrato_173/model/application/Program.java
@@ -6,8 +6,8 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Shape circle = new Circle(Color.WHITE, 5.3);
-		Shape rectangle = new Rectangle(Color.BLACK, 5.3, 3.3);
+		AbstractShape circle = new Circle(Color.WHITE, 5.3);
+		AbstractShape rectangle = new Rectangle(Color.BLACK, 5.3, 3.3);
 		System.out.println("This is a area of circle: " + circle.area() + ". And this is a color of this circle: " + circle.getColor());
 		System.out.println("This is a area of rectangle: " + rectangle.area() + ". And this is a color of this rectangle: " + rectangle.getColor());
 		
--- curso_programacao/section_16/herdar_Vs_Cumprircontrato_173/model/entities/AbstractShape.java
+++ curso_programacao/section_16/herdar_Vs_Cumprircontrato_173/model/entities/AbstractShape.java
@@ -1,21 +1,22 @@
 package model.entities;
 
 import model.enums.Color;
+import model.interfaces.Shape;
 
-public abstract class Shape {
+public abstract class AbstractShape implements Shape {
 
 	private Color color;
-	
-	public abstract double area();
 	
 	public Color getColor() {
 		return color;
 	}
 
-	public Shape(Color color) {
+	public AbstractShape(Color color) {
 		super();
 		this.color = color;
-	} 
+	}
+	
+	public abstract double area();
 	
 	
 }
