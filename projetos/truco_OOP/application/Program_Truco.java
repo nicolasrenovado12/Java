dd72e60cf04f3d40fad932c2091940f9a952368e
nicolasrenovado12 <nicolasrenovado12@gmail.com>
2025-01-24 21:03:13 -0300

    Add logic of moves

--- projetos/truco_OOP/application/Program_Truco.java
+++ projetos/truco_OOP/application/Program_Truco.java
@@ -1,12 +1,12 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Random;
 import java.util.Scanner;
 
-import entities.Letters;
 import entities.Player;
 import services.Random_Matriz_Service;
 
@@ -37,22 +37,76 @@ public class Program_Truco {
    
         for(int i=0; i<symbols.size(); i++) {
         	for(int e=1; e<=suit.size(); e++) {
-        		deck.put(cardNumber, symbols.get(i)+suit.get(e));
+        		deck.put(cardNumber, symbols.get(i)+" "+suit.get(e));
         		cardNumber++;
         	}
         }
-        
+        System.out.println(deck);
+        System.out.println("Digit your name: ");
+        String name = sc.next();        
         Map<Integer, String> playerCards = new HashMap<>(); 
+        Map<Integer, String> machineCards = new HashMap<>();
         
-        /*
-        Random_Matriz_Service.randomizerMap(ranksPlayer, ranks, 3);
-        Random_Matriz_Service.randomizerMap(suitsPlayer, suits, 3);
+        Random_Matriz_Service.randomizerMap(playerCards, deck, 3);
+        Random_Matriz_Service.randomizerMap(machineCards, deck, 3);
+        List<Integer> keysMachine = new ArrayList<>();
+        List<Integer> keysPlayer = new ArrayList<>();
+
         
-        Letters letters = new Letters(ranksPlayer, suitsPlayer);;              
+        Player player = new Player(name, playerCards);
+        
+        machineCards.forEach((key, value) -> {
+        	keysMachine.add(key);
+        	});
         playerCards.forEach((key, value) -> {
-            System.out.println("Chave: " + key + " Valor: " + value);
-            });        
-          */      
+        	keysPlayer.add(key);
+        	});
+        
+        boolean game = false;
+        while(game == false) {
+
+        	int machineGame = random.nextInt(machineCards.size());
+            System.out.println("Your cards");
+            playerCards.forEach((key, value) -> {
+               	
+                	System.out.println("Chave: " + keysPlayer.indexOf(key) + " Valor: " + value);
+                   	
+
+                });
+;
+
+            System.out.println("Digit value of your card: ");
+            int playerGame = sc.nextInt();
+           
+            if (playerCards.get(keysPlayer.get(playerGame)) != null) {
+            	System.out.println("Your card:");
+            	System.out.println(playerCards.get(keysPlayer.get(playerGame)));
+               	playerCards.remove(keysPlayer.get(playerGame));
+               	
+              
+                System.out.println("Card of machine: ");
+                System.out.println(machineCards.get(keysMachine.get(machineGame)));
+                machineCards.remove(keysMachine.get(machineGame));
+                
+                
+                if(keysPlayer.get(playerGame) > keysMachine.get(machineGame)) {
+                    System.out.println("Player win");
+                    keysMachine.remove(keysMachine.get(machineGame));
+                    keysPlayer.remove(keysPlayer.get(playerGame));
+                    System.out.println();
+                    
+                } else {
+                    System.out.println("Machine win");
+                    keysMachine.remove(keysMachine.get(machineGame));
+                    keysPlayer.remove(keysPlayer.get(playerGame));
+                    System.out.println();
+                
+                }
+
+            } else {
+            	System.out.println("Digite um valor válido");
+            }
+        }    
                 
                 
     
